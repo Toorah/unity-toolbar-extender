b37d75fa05c1d43dcc70e1fb2f2f8e6e911a0044
Toorah <tugratoorah@gmail.com>
2020-08-26 18:28:56 +0200

    Custom Toolbar to restart playmode

---
+++ Editor/CustomToolbar.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityToolbarExtender;
+
+[InitializeOnLoad]
+public class CustomToolbar
+{
+	static class ToolbarStyles
+	{
+		public static readonly GUIStyle commandButtonStyle;
+
+		static ToolbarStyles()
+		{
+			commandButtonStyle = new GUIStyle("AppCommand")
+			{
+				fontSize = 14,
+				alignment = TextAnchor.MiddleCenter,
+				imagePosition = ImagePosition.ImageAbove,
+				fontStyle = FontStyle.Bold
+			};
+		}
+	}
+
+	static Texture2D m_icon;
+
+	static CustomToolbar()
+	{
+		m_icon = Resources.Load<Texture2D>("renew");
+
+		ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+		if (EditorPrefs.GetBool("Start-Playing", false))
+			DoPlay();
+	}
+
+	static void OnToolbarGUI()
+	{
+		GUILayout.FlexibleSpace();
+
+		GUI.enabled = EditorApplication.isPlaying;
+
+		if (GUILayout.Button(new GUIContent(m_icon, "Restart"), ToolbarStyles.commandButtonStyle))
+		{
+			EditorApplication.ExitPlaymode();
+			EditorPrefs.SetBool("Start-Playing", true);
+            EditorApplication.playModeStateChanged += TryPlayAgain;
+		}
+
+		GUI.enabled = true;
+	}
+
+    private static void TryPlayAgain(PlayModeStateChange state)
+    {
+        switch (state)
+        {
+            case PlayModeStateChange.EnteredEditMode:
+				DoPlay();
+				break;
+        }
+    }
+
+	private static void DoPlay()
+    {
+		EditorApplication.EnterPlaymode();
+		EditorApplication.playModeStateChanged -= TryPlayAgain;
+		EditorPrefs.SetBool("Start-Playing", false);
+	}
+}
